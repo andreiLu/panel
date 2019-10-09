00e0b5b6e06d9eebb7be16d2cba360a53eff22c8
andreiL <lucaandrei_13@yahoo.ro>
2019-10-10 00:16:46 +0300

    added vue.js

---
+++ assets/js/frontend/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="test">
+
+        here we go
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
